0944c3b0a3fa7342e31bf96826c0fee3a7c0569c
Anton <anton.vosc@gmail.com>
2024-07-06 17:18:20 +0100

    nearly added main logic

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var origBoard;
 const huPlayer = 'O';
 const aiPlayer = 'X';
+
 const winCombos = [
 	[0, 1, 2],
 	[3, 4, 5],
@@ -13,11 +14,13 @@ const winCombos = [
 ]
 
 const cells = document.querySelectorAll('.cell');
+
 startGame();
 
 function startGame() {
 	document.querySelector(".endgame").style.display = "none";
 	origBoard = Array.from(Array(9).keys());
+
 	for (var i = 0; i < cells.length; i++) {
 		cells[i].innerText = '';
 		cells[i].style.removeProperty('background-color');
@@ -32,4 +35,31 @@ function turnClick(square) {
 function turn(squareId, player) {
 	origBoard[squareId] = player;
 	document.getElementById(squareId).innerText = player;
+	let gameWon = checkWin(origBoard, player)
+
+	if (gameWon) gameOver(gameWon)
+}
+
+function checkWin(board, player) {
+	let plays = board.reduce((a, e, i) => (e === player) ? a.concat(i) : a, []);
+	let gameWon = null;
+
+	for (let [index, win] of winCombos.entries()) {
+		if (win.every(elem => plays.indexOf(elem) > -1)) {
+			gameWon = {index: index, player: player};
+			break;
+		}
+	}
+
+	return gameWon;
+}
+
+function gameOver(gameWon) {
+	for (let index of winCombos[gameWon.index]) {
+		document.getElementById(index).style.backgroundColor = gameWon.player == huPlayer ? "blue" : "red";
+	}
+
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].removeEventListener('click', turnClick, false)
+	}
 }
